75a800f094fcd78af1cb90113c0d471a26336201
ELCBAh <6211@holbertonstudents.com>
2023-07-05 20:20:54 +0000

    base_model.py updated and added necessary functionalities

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Base Model"""
 import uuid
-import datetime
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -12,7 +13,27 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        storage.new(self)
 
     def update_timestamp(self):
         """Update datetime"""
         self.updated_at = datetime.now()
+
+    def __srt__(self):
+        """String representation"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+
+    def save(self):
+        """Save method"""
+        self.update_timestamp()
+        storage.save()
+
+    def to_dict(self):
+        """Convert to dictionary"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
