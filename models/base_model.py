ab685dcf99fbccce05f0f9b7427787ff3f754a39
ELCBAh <6211@holbertonstudents.com>
2023-07-06 14:03:13 +0000

    modified Basemodel to handle args and kwargs and updates value contents

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,21 @@ from models import storage
 class BaseModel:
     """Base Model Class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization (generate id and datetime)"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        storage.new(self)
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key is not "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
-    def update_timestamp(self):
+    def update_timestamp(self): 
         """Update datetime"""
         self.updated_at = datetime.now()
 
