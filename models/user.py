82111c766387c5c15696f73efe9aa0281e079855
ELCBAh <6211@holbertonstudents.com>
2023-07-08 23:03:11 +0000

    modified user to convert to dictionary as expected

--- models/user.py
+++ models/user.py
@@ -18,3 +18,23 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+            Constructor.
+        """
+        super().__init__(*args, **kwargs)
+    
+    def to_dict(self):
+        """
+            Return a dictionary representation of a User.
+        """
+        return {
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+            "email": self.email,
+            "password": self.password,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+        }
