5ce97a0b3eb580228f8ae3ebd20a0069e888af37
ELCBAh <6211@holbertonstudents.com>
2023-07-08 19:50:46 +0000

    fixed console problems, adding diffferent functionalities

--- console.py
+++ console.py
@@ -30,6 +30,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
+        """Creates new instance of existing class"""
         if not arg:
             print("** class name missing **")
             return
@@ -40,22 +41,23 @@ class HBNBCommand(cmd.Cmd):
             model = BaseModel()
             print(model.id)
 
-    def do_show(self, *args):
-
-        model = BaseModel()
-        if not args:
+    def do_show(self, arg):
+        """Show specified class instance"""
+        if not arg:
             print("** class name missing **")
             return
+        args = arg.split()
         if args[0] != "BaseModel":
             print("** class doesn't exist **")
             return
-        if len(args) == 1:
+        if len(args) < 2:
             print("** instance id missing **")
             return
-        if args[1] != model.id:
+        model_id = args[1]
+        if model_id not in storage.get(args[0], model_id):
             print("** no instance found **")
-            return
-
+        else:
+            print(storage.all()[model_id])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
